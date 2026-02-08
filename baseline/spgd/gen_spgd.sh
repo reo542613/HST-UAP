4709bd43b594e460e80d0c722f166a7d0f26a5ce
reo542613 <2251185589@qq.com>
2026-02-08 23:05:53 +0800

    Update gen_spgd.sh

--- baseline/spgd/gen_spgd.sh
+++ baseline/spgd/gen_spgd.sh
@@ -1,11 +1,11 @@
-#CUDA_VISIBLE_DEVICES='2' python imagenet_attack.py --data_dir /mnt/igps_622/la/imagenet/train/ --uaps_save /mnt/igps_622/la/SPGDandSGA/spgd/VITL/ --batch_size 125 --alpha 10 --epoch 20 --spgd 1 --num_images 10000 --model_name ViT-L --Momentum 0 --cross_loss 0
+#CUDA_VISIBLE_DEVICES='2' python imagenet_attack.py --data_dir /mnt/imagenet/train/ --uaps_save /mnt/SPGDandSGA/spgd/VITL/ --batch_size 125 --alpha 10 --epoch 20 --spgd 1 --num_images 10000 --model_name ViT-L --Momentum 0 --cross_loss 0
 
 
 #!/bin/bash
 # 顺序训练 AlexNet / GoogLeNet / VGG16 / VGG19 / ResNet152
 models=("AlexNet" "GoogLeNet" "VGG16" "VGG19" "ResNet152")
 models=( "VGG19" "ResNet152")
-base_dir="/mnt/igps_622/la/SPGDandSGA/spgd"
+base_dir="/mnt/SPGDandSGA/spgd"
 
 for m in "${models[@]}"; do
     echo "========== Training $m =========="
@@ -13,7 +13,7 @@ for m in "${models[@]}"; do
     mkdir -p "$save_path"
 
     CUDA_VISIBLE_DEVICES=1 python imagenet_attack.py \
-        --data_dir /mnt/igps_622/la/imagenet/train/ \
+        --data_dir /mnt/imagenet/train/ \
         --uaps_save "$save_path" \
         --batch_size 125 \
         --alpha 10 \
